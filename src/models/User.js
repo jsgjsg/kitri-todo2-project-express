af8dff7bdfb34ce1adcf4672479303f1603fef9b
Jeon Seung Kyu <jsgcys0815@gmail.com>
2024-06-25 11:14:52 +0900

    /auth 기능 수정 (username =>  email)

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  username : { type: String, required: true, unique:true},
+  email : { type: String, required: true, unique:true},
   password : { type: String, requierd: true},
-  age : { type: Number, require: true},
+  // age : { type: Number, require: true},
   refreshToken: { type: String }
 });
 
--- src/strategies/localStrategy.js
+++ src/strategies/localStrategy.js
@@ -1,14 +1,13 @@
 import { Strategy as LocalStrategy } from 'passport-local';
-// import { users } from '../users.js';
 import User from '../models/User.js';
 
-const localStrategy = new LocalStrategy({ usernameField: 'username' },
-  async (username, password, done) => {
-    // const user = users.find(user => user.username === username);
-    const user = await User.findOne({username});
+const localStrategy = new LocalStrategy({ usernameField: 'email' },
+  async (email, password, done) => {
+    // const user = users.find(user => user.email === email);
+    const user = await User.findOne({email});
     console.log(user);
     if (!user) {
-      return done(null, false, { message: 'Incorrect username.' });
+      return done(null, false, { message: 'Incorrect email.' });
     }
 
     const isMatch = user.password == password;
